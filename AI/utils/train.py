c2edca9c30762994fcc15b451c079819c72162d0
reza-shahriari <rezshahriari@gmail.com>
2025-01-06 20:40:00 +0330

    update train.

--- AI/utils/train.py
+++ AI/utils/train.py
@@ -5,18 +5,32 @@ just get the name as input and run the training process.
 
 from ultralytics import YOLO
 
-def train_yolo11_model():
-    model = YOLO('yolo11m')
+def train_yolo_model(model_size = 'l',version='11'):
+    model_size = model_size.lower()
+    while model_size not in ['n','s', 'm', 'l', 'x']:
+        print('model size must be s, m, l, x')
+        model_size = input('model size: ').strip().lower()
+    while version not in ['11','8']:
+        print('version must be 11, 8, 10 ')
+        version = input('version: ').strip()
+    name = 'yolov'+ version + model_size + '.pt'
+    model = YOLO(name)
+    batch_size = -1
+    if model_size =='n':
+        batch_size = 16
+
     model.train(
         data="AI/yolo_configs/carDD.yaml",
         epochs=100,
         imgsz=640,
-        batch=-1,
+        batch=batch_size,
     )
 
 
 
 if __name__ == '__main__':
-    model_name = 'yolo11'
-    if model_name=='yolo11':
-        train_yolo11_model()
+    model_name = 'yolo'
+    version = '8'
+    model_size = 'n'
+    if model_name=='yolo':
+        train_yolo_model(model_size=model_size,version=version)
